29209d33e3fed53475829914eb0c54aff2889c7b
shdbl <102776480+shdbl@users.noreply.github.com>
2023-05-02 13:59:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,59 +1,58 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
-# 展示文本；文本直接使用Markdown语法
-st.markdown("# Streamlit示例")
-st.markdown("""
-            - 这是
-            - 一个
-            - 无序列表
-            """)
+df = pd.read_csv('D:/处理后的聊天记录.txt', sep='\t', header=None, names=['sender', 'message'])
 
-# 展示pandas数据框
-if st.checkbox('Show raw data'):
-    st.dataframe(pd.DataFrame([[1, 2], [3, 4]], columns=["a", "b"]))
+from sklearn.feature_extraction.text import CountVectorizer
+import jieba
+
+def jieba_tokenizer(text):
+    return jieba.lcut(text)
+
+vectorizer = CountVectorizer(tokenizer=jieba_tokenizer)
+X=df['message']
+X=X.fillna('')
+X = vectorizer.fit_transform(X)
+y = df['sender']
+unique_speakers = list(set(y.values))
+speaker_labels = [unique_speakers.index(speaker) for speaker in y.values]
+
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+X_train, X_test, y_train, y_test = train_test_split(X, speaker_labels, test_size=0.2, random_state=2)
+
+clf = MultinomialNB()
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+score = accuracy_score(y_test, y_pred)
+print(score)
+
+import re
+
+def remove_bracket_characters(s):
+    # 使用正则表达式替换s中的[]和其中的内容
+    result = re.sub(r'\[.*?\]', '', s)
+    # punctuation='[!@#$%^&*()_+-={}|:"\';<>,，。？！“”：.?/\\\[\]]'
+    # result=re.sub(punctuation,'',result)
+    return result
+
+def shibie(s):
+    new_message = [remove_bracket_characters(s)]
+    new_message_transformed = vectorizer.transform(new_message)
+    probability = clf.predict_proba(new_message_transformed)[0]
+    for i in range(len(unique_speakers)):
+        print(unique_speakers[i] + ':'+'{:.2f}%'.format(100*probability[i]))
 
 
-# 加入交互控件，如输入框
-number = st.number_input("Insert a number", 1)
-st.write("输入的数字是：", number)
+st.title("发言者检测")
 
-# 展示matplotlib绘图
-fig=plt.figure(figsize=(5,number))
-arr = np.random.normal(1, 1, size=100)
-plt.hist(arr, bins=20)
-plt.title("matplotlib plot")
-st.pyplot(fig)
-st.title("Streamlit 手动刷新示例")
+user_input = st.text_input("请输入一句话:")
 
-if st.button("刷新"):
-    st.experimental_rerun()
+prob=shibie(user_input)
 
-#
-st.title('Streamlit 数学公式示例')
-
-st.markdown(
-    r'''
-MathJax 可以帮助我们渲染数学公式。 下面是一个公式的例子:
-
-$$
-f(x) = \int_{-\infty}^\infty \hat f(\xi)\,e^{2 \pi i \xi x} \,d\xi
-$$
-
-你还可以使用行内公式，例如当 $a \ne 0$ 时，二次方程 $ax^2 + bx + c = 0$ 的解为：
-
-$$
-x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}
-$$
-'''
-)
-
-st.title("文件上传示例")
-
-uploaded_file=st.file_uploader("选择一个 CSV 文件",type="csv")
-
-if uploaded_file is not None:
-    data=pd.read_csv(uploaded_file)
-    st.write(data)
+st.write("发言者概率:")
+st.write(prob)
