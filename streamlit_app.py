d38a8db16506838cd6bf687378f97445f52f9156
shdbl <102776480+shdbl@users.noreply.github.com>
2023-05-21 16:43:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,18 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
+import openai
 
-df = pd.read_csv('./ltjl.txt', sep='\t', header=None, names=['sender', 'message'])
+# 设置 API 密钥
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-from sklearn.feature_extraction.text import CountVectorizer
-import jieba
+def generate_text(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=100,  # 调整此数值以生成不同长度的文本
+        n=1,
+        temperature=0.5,
+    )
+    return response.choices[0].text.strip()
 
-def jieba_tokenizer(text):
-    return jieba.lcut(text)
-
-vectorizer = CountVectorizer(tokenizer=jieba_tokenizer)
-X=df['message']
-X=X.fillna('')
-X = vectorizer.fit_transform(X)
-y = df['sender']
-unique_speakers = list(set(y.values))
-speaker_labels = [unique_speakers.index(speaker) for speaker in y.values]
-
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
-
-X_train, X_test, y_train, y_test = train_test_split(X, speaker_labels, test_size=0.2, random_state=2)
-
-clf = MultinomialNB()
-clf.fit(X_train, y_train)
-
-y_pred = clf.predict(X_test)
-score = accuracy_score(y_test, y_pred)
-print(score)
-
-import re
-
-def remove_bracket_characters(s):
-    # 使用正则表达式替换s中的[]和其中的内容
-    result = re.sub(r'\[.*?\]', '', s)
-    # punctuation='[!@#$%^&*()_+-={}|:"\';<>,，。？！“”：.?/\\\[\]]'
-    # result=re.sub(punctuation,'',result)
-    return result
-
-def shibie(s):
-    new_message = [remove_bracket_characters(s)]
-    new_message_transformed = vectorizer.transform(new_message)
-    probability = clf.predict_proba(new_message_transformed)[0]
-    s=''
-    for i in range(len(unique_speakers)):
-        s+=unique_speakers[i] + ':'+'{:.2f}%'.format(100*probability[i])+'\n\n'
-    return s
-
-
-
-st.title("发言者检测")
-
-user_input = st.text_input("请输入一句话:")
-
-prob=shibie(user_input)
-
-st.write("发言者概率:")
-st.write(prob)
+prompt = "Translate the following English text to French: '"
+generated_text = generate_text(prompt)
+print(generated_text)
